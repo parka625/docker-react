0190cf4784a06aee3b0b4b195cc017979ff5bf1e
Ian <parka625@gmail.com>
2023-05-30 00:02:47 +1000

    reset text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          sdfsdfdfdfsdsdf
+          It is amazing when things work. 
         </p>
         <a
           className="App-link"
